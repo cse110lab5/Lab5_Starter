7268d2b7d8eff394d70ae0f055a85fd6e907b5f5
pedrosh <serdiopedro@gmail.com>
2024-05-01 15:33:51 -0700

    layout for expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,23 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  htmlElement.addEventListener('input', function() {
+    let soundName = document.getElementById('horn-select');
+    let image = document.getElementsByClassName('img')
+
+    if(soundName == 'air-horn'){
+      
+      //load the sound
+    }
+
+    if(soundName == 'car-horn'){
+      let image = document.getElementsByClassName('img')
+      //load the sound
+    }
+
+    if(soundName == 'party-horn'){
+      let image = document.getElementsByName('img')
+      //load the sound
+    }
+  })
 }
