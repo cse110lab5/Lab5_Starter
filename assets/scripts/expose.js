f53247b24a4bb76cd9b240d713a822150ee91b5b
pedrosh <serdiopedro@gmail.com>
2024-05-01 20:02:42 -0700

    got the image change to work

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,20 +6,21 @@ function init() {
   let hornOptions = document.getElementById('horn-select');
 
   hornOptions.addEventListener('change', function() {
-    let optionSound = hornOptions.options[hornOptions.selectedIndex].text;
-
-    if(optionSound == 'air-horn'){
-      //image.src = 'assets/images/air-horn.svg';
+    let optionSound = hornOptions.value;
+    let image = document.getElementsByTagName('img'); //need to get image source
+    console.log(optionSound)
+    if(optionSound === 'air-horn'){
+      image[0].src = 'assets/images/air-horn.svg';
       //load the sound
     }
 
-    if(optionSound == 'car-horn'){
-      // change image
+    if(optionSound === 'car-horn'){
+      image[0].src = 'assets/images/car-horn.svg';
       //load the sound
     }
 
-    if(optionSound == 'party-horn'){
-      //change image
+    if(optionSound === 'party-horn'){
+      image[0].src = 'assets/images/party-horn.svg';
       //load the sound
     }
   })
