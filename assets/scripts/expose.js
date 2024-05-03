07d501c874913ad46873c5e8d2fe272131b8e03a
pedrosh <serdiopedro@gmail.com>
2024-05-02 23:25:11 -0700

    Refactor expose.js for image and volume functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,46 +3,67 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  const jsConfetti = new JSConfetti();
   let hornOptions = document.getElementById('horn-select');
   let button = document.querySelector('button');
+  let slider = document.getElementById('volume');
+  let sliderValue = slider.value;
   let sound = document.querySelector('.hidden');
-  let image = document.querySelector('img');
+  let image = document.getElementsByTagName('img');
   let isParty = false;
 
   hornOptions.addEventListener('change', function() {
     let hornEl = hornOptions.value;
-  
+    let hornTypeImage = image[0];
     if(hornEl === 'air-horn'){
       isParty = false;
-      image.src = 'assets/images/air-horn.svg';
+      hornTypeImage.src = 'assets/images/air-horn.svg';
       sound.src = 'assets/audio/air-horn.mp3';
     }
 
     else if(hornEl === 'car-horn'){
       isParty = false;
-      image.src = 'assets/images/car-horn.svg';
+      hornTypeImage.src = 'assets/images/car-horn.svg';
       sound.src = 'assets/audio/car-horn.mp3';
     }
 
      else if(hornEl === 'party-horn'){
       isParty = true;
-      image.src = 'assets/images/party-horn.svg';
+      hornTypeImage.src = 'assets/images/party-horn.svg';
       sound.src = 'assets/audio/party-horn.mp3';
     }
   });
 
+  ///also change audio volume
+  slider.addEventListener('input', function(){
+    sliderValue = slider.value;
+    let speakerImage = image[1];
+    if (sliderValue == 0){
+      speakerImage.src =  'assets/icons/volume-level-0.svg';
+
+    } else if (sliderValue >= 1 && sliderValue < 33){
+      speakerImage.src =  'assets/icons/volume-level-1.svg';
+
+    } else if (sliderValue >=33 && sliderValue < 66){
+      speakerImage.src =  'assets/icons/volume-level-2.svg';
+
+    } else{
+      speakerImage.src =  'assets/icons/volume-level-3.svg';
+    }
+  })
+
   button.addEventListener('click', function() {
     if(isParty){
-      const jsConfetti = new JSConfetti();
       jsConfetti.addConfetti({
-        emojis: ['🎉'],
+        emojis: ['🎉','🎊','🥳'],
         emojiSize: 50,
         confettiNumber: 50,
-      })
+      });
     }
+    sound.volume = sliderValue / 100;
     sound.play();
   });
 
-  //Add slider functionality below here
-  
+
+
 }
