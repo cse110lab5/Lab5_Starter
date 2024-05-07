ace06041b1bef9fcb0a1efe3d8cf71ab611d9206
Ryan-izad <117335821+Ryan-izad@users.noreply.github.com>
2024-05-07 13:35:51 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,32 +1,25 @@
+// explore.js
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   let synth = window.speechSynthesis;
+  let voices = synth.getVoices();
   let voiceSelect = document.getElementById('voice-select');
   let textInput = document.getElementById('text-to-speak');
   let playButton = document.querySelector('button');
   let image = document.querySelector('img');
 
-  // Populate the dropdown menu with voices
-  function populateVoices() {
-    let voices = synth.getVoices();
-    for (let i = 0; i < voices.length; i++) {
-      const option = document.createElement("option");
-      option.textContent = `${voices[i].name} (${voices[i].lang})`;
-      option.setAttribute("data-lang", voices[i].lang);
-      option.setAttribute("data-name", voices[i].name);
-      voiceSelect.appendChild(option);
-    }
-  }
+  //populate the dropdown menue with voices
+  // -> voices are loading only after a refresh in inital page open
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-  // Ensure voices are populated before initializing
-  if (synth.onvoiceschanged !== undefined) {
-    synth.onvoiceschanged = populateVoices;
-  } else {
-    // If onvoiceschanged is not supported, populate voices immediately
-    populateVoices();
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
   }
-
   voiceSelect.addEventListener('change', function() {
     voiceSelect.value = this.value;
   });
@@ -38,7 +31,6 @@ function init() {
   playButton.addEventListener('click', function() {
     let utterThis = new SpeechSynthesisUtterance(textInput.value);
     let selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-    let voices = synth.getVoices();
     for (let i = 0; i < voices.length; i++) {
       if (voices[i].name === selectedOption) {
         utterThis.voice = voices[i];
